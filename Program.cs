d3ad5240f23f2084f489f805a73045ed796c34dc
Juan Crisóstomo <juancvfenix@gmail.com>
2016-11-09 23:53:53 -0600

    hola mundo, microsoft dot-net-core

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, " + args[0] + " : " + DateTime.Now.DayOfWeek);
+        }
+    }
+}
